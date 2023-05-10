133d5371bb2e7e7cb1aef99162e3e0ab5f6fb62b
phuc.letan <tomdwanno1@gmail.com>
2023-05-10 09:21:46 +0700

    Search Binary Problem

---
+++ src/com/phucdevs/leetcode/recursion/SearchBinary.java
@@ -0,0 +1,46 @@
+package com.phucdevs.leetcode.recursion;
+
+public class SearchBinary {
+
+    public static void main(String[] args) {
+
+        /**
+         * You are given the root of a binary search tree (BST) and an integer val.
+         *
+         * Find the node in the BST that the node's value equals val and return
+         * the subtree rooted with that node. If such a node does not exist, return null.
+         *
+         * Input: root = [4,2,7,1,3], val = 2
+         * Output: [2,1,3]
+         *
+         * Input: root = [4,2,7,1,3], val = 5
+         * Output: []
+         */
+    }
+
+    static TreeNode searchBST(TreeNode root, int val) {
+
+        if (root == null || root.val == val) {
+            return root;
+        }
+
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        }
+
+        return searchBST(root.right, val);
+    }
+}
+
+ class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+             this.left = left;
+             this.right = right;
+     }
+}
